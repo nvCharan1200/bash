f919d9720b937ab06ac43ccf43980eb6715a3422
nvCharan1200 <charandds2020@gmail.com>
2025-11-15 23:24:13 +0530

    second

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update –y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+# Add required dependencies for the jenkins package
+    sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
